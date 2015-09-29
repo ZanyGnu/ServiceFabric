46cc69a8ce38ecc63b41067885649215fe8d54d3
ZanyGnu <ajaymartin@gmail.com>
2015-09-28 21:48:50 -0700

    basic template for stateful actor service

---
+++ ServiceFabricApplication.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using StatefulActorService.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFabricApplication.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var proxy = ActorProxy.Create<IStatefulActorService>(ActorId.NewId(), "fabric:/ServiceFabricApplication");
+
+            int count = 10;
+            Console.WriteLine("Setting Count to in Actor {0}: {1}", proxy.GetActorId(), count);
+            proxy.SetCountAsync(count).Wait();
+
+            Console.WriteLine("Count from Actor {0}: {1}", proxy.GetActorId(), proxy.GetCountAsync().Result);
+        }
+    }
+}
---
+++ StatefulActorService/StatefulActorServiceState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using StatefulActorService.Interfaces;
+using Microsoft.ServiceFabric;
+using Microsoft.ServiceFabric.Actors;
+
+namespace StatefulActorService
+{
+    [DataContract]
+    public class StatefulActorServiceState
+    {
+        [DataMember]
+        public int Count;
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "StatefulActorServiceState[Count = {0}]", Count);
+        }
+    }
+}
